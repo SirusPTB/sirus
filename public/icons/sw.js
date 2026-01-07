6ef8a4ca8da1db7960365d94c6b109b56d6da92a
SirusPTB <144066355+SirusPTB@users.noreply.github.com>
2026-01-08 10:52:02 +1100

    Add files via upload

---
+++ public/icons/sw.js
@@ -0,0 +1,48 @@
+// sw.js
+const CACHE_NAME = 'lvr1-cache-v1';
+const urlsToCache = [
+  '/',
+  'index.html',
+  // 'DSEG7Classic-Bold.woff2',
+  // 'DSEG7Classic-Bold.woff',
+  // 'DSEG7Classic-Bold.ttf'
+  // Add any other assets like CSS/JS if separated, or additional fonts/images
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
