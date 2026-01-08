32ba294b0d29e55e97e4e324c83637320a52816e
SirusPTB <144066355+SirusPTB@users.noreply.github.com>
2026-01-08 14:16:54 +1100

    Add files via upload

--- public/icons/sw.js
+++ public/icons/sw.js
@@ -1,48 +1,45 @@
 // sw.js
 const CACHE_NAME = 'lvr1-cache-v1';
+
+// Use relative paths (./) so it works in subfolders like /sirus/public/
 const urlsToCache = [
-  '/',
-  'index.html',
-  // 'DSEG7Classic-Bold.woff2',
-  // 'DSEG7Classic-Bold.woff',
-  // 'DSEG7Classic-Bold.ttf'
-  // Add any other assets like CSS/JS if separated, or additional fonts/images
+  './',                  // Cache the current directory (helps with the root page)
+  './index.html',
+  './manifest.json',
+  // './DSEG7Classic-Bold.woff2',   // Uncomment once fonts are confirmed working
+  // './DSEG7Classic-Bold.woff',
+  // './DSEG7Classic-Bold.ttf'
+  // Add icons if you have an 'icons' folder:
+  // './icons/icon-192x192.png',
+  // './icons/icon-512x512.png'
 ];
 
-self.addEventListener('install', (event) => {
+self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then((cache) => {
-        console.log('Opened cache');
-        return cache.addAll(urlsToCache);
-      })
+      .then(cache => cache.addAll(urlsToCache))
+      .then(() => self.skipWaiting())
   );
 });
 
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request)
-      .then((response) => {
-        // Cache hit - return response
-        if (response) {
-          return response;
-        }
-        return fetch(event.request);
-      })
-  );
-});
-
-self.addEventListener('activate', (event) => {
-  const cacheWhitelist = [CACHE_NAME];
+self.addEventListener('activate', event => {
   event.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return Promise.all(
-        cacheNames.map((cacheName) => {
-          if (cacheWhitelist.indexOf(cacheName) === -1) {
-            return caches.delete(cacheName);
+    caches.keys().then(names =>
+      Promise.all(
+        names.map(name => {
+          if (name !== CACHE_NAME) {
+            return caches.delete(name);
           }
         })
-      );
+      )
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(resp => {
+      return resp || fetch(event.request);
     })
   );
 });
