9430e43d87d7228a896fe0e483d839f4746acc08
SirusPTB <144066355+SirusPTB@users.noreply.github.com>
2026-01-14 13:16:20 +1100

    Add files via upload

--- sw.js
+++ sw.js
@@ -10,24 +10,43 @@ const URLS_TO_CACHE = [
   'icons/icon-192x192.png',
   'icons/icon-512x512.png',
   'favicon.ico'
-  // Add 'setup.html' if you have it
 ];
 
+// Install
 self.addEventListener('install', event => {
+  self.skipWaiting(); // force install immediately
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(URLS_TO_CACHE))
-      .then(() => self.skipWaiting())
+    caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL))
   );
 });
 
+// Activate
 self.addEventListener('activate', event => {
-  event.waitUntil(self.clients.claim());
+  event.waitUntil(
+    Promise.all([
+      caches.keys().then(keys =>
+        Promise.all(
+          keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
+        )
+      ),
+      self.clients.claim() //  take control immediately
+    ])
+  );
 });
 
+// Fetch (network-first)
 self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+
   event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
+    fetch(event.request)
+      .then(res => {
+        const copy = res.clone();
+        caches.open(CACHE_NAME).then(cache =>
+          cache.put(event.request, copy)
+        );
+        return res;
+      })
+      .catch(() => caches.match(event.request))
   );
 });
